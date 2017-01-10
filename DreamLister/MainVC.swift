23366fd848a5addeac611b1888d7194fd9448d8b
nguyenhungnq <NguyenHung@antonio.local>
2017-01-10 17:48:17 +0700

    Designing the view

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by NguyenHung on 1/10/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
