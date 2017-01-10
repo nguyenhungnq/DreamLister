3b7cae45489f93c2f3e2f1b6e19486a5db4df6c1
nguyenhungnq <NguyenHung@antonio.local>
2017-01-10 14:16:14 +0700

    data

---
+++ DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by NguyenHung on 1/10/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
---
+++ DreamLister/ItemType+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  ItemType+CoreDataProperties.swift
+//  DreamLister
+//
+//  Created by NguyenHung on 1/10/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import 
+
+extension ItemType {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ItemType> {
+        return NSFetchRequest<ItemType>(entityName: "ItemType");
+    }
+
+    @NSManaged public var type: String?
+    @NSManaged public var toItem: Item?
+
+}
