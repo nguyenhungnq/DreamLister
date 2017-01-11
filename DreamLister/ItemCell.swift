ab1a692ef343d9a792aa123117094c72a50a5ab5
nguyenhungnq <NguyenHung@antonio.local>
2017-01-11 09:38:09 +0700

    Connecting the Views

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by NguyenHung on 1/11/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
