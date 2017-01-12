e2d7867dc98428048a66797acaabc6e7b27481b2
nguyenhungnq <NguyenHung@antonio.local>
2017-01-12 14:34:29 +0700

    Laying Out The Details Screen

---
+++ DreamLister/ItemDetailsVC.swift
@@ -0,0 +1,40 @@
+//
+//  ItemDetailsVC.swift
+//  DreamLister
+//
+//  Created by NguyenHung on 1/12/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+            //Dong nay thay doi mau nut back khi chuyen sang man hinh thu 3
+        }
+      
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
